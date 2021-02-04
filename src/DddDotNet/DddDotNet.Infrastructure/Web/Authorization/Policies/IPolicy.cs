2e54b2ce2a881ba3b9ee8f3a656083c0fe059cdf
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-02-04 10:31:09 +0700

    Access Control

---
+++ src/DddDotNet/DddDotNet.Infrastructure/Web/Authorization/Policies/IPolicy.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace DddDotNet.Infrastructure.Web.Authorization.Policies
+{
+    public interface IPolicy
+    {
+        void Configure(AuthorizationPolicyBuilder policy);
+    }
+}
