4874e02a9a431f7fc4222007076e9a0feeebae09
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-08-25 22:24:03 +0700

    [IntegrationTest] Azure Storage Queue

---
+++ src/DddDotNet/DddDotNet.IntegrationTests/Infrastructure/MessageBrokers/AzureQueueSenderTests.cs
@@ -0,0 +1,33 @@
+﻿using DddDotNet.Domain.Infrastructure.MessageBrokers;
+using DddDotNet.Infrastructure.MessageBrokers.AzureQueue;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DddDotNet.IntegrationTests.Infrastructure.MessageBrokers
+{
+    public class AzureQueueSenderTests
+    {
+        private static string _connectionString;
+
+        public AzureQueueSenderTests()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("09f024f8-e8d1-4b78-9ddd-da941692e8fa")
+                .Build();
+
+            _connectionString = config["MessageBroker:AzureQueue:ConnectionString"];
+        }
+
+        [Fact]
+        public async Task SendAsync_Success()
+        {
+            var message = new { Id = Guid.NewGuid() };
+            var metaData = new MetaData { };
+            var azureQueueSender = new AzureQueueSender<object>(_connectionString, "integration-test");
+            await azureQueueSender.SendAsync(message, metaData);
+        }
+    }
+}
