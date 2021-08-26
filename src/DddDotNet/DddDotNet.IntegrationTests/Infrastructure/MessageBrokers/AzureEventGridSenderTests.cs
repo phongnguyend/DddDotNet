3cfdc7c59f7829db29a1e94d4c61eaf76088a857
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-08-26 23:08:36 +0700

    [IntegrationTest] Azure Event Grid

---
+++ src/DddDotNet/DddDotNet.IntegrationTests/Infrastructure/MessageBrokers/AzureEventGridSenderTests.cs
@@ -0,0 +1,35 @@
+﻿using DddDotNet.Domain.Infrastructure.MessageBrokers;
+using DddDotNet.Infrastructure.MessageBrokers.AzureEventGrid;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DddDotNet.IntegrationTests.Infrastructure.MessageBrokers
+{
+    public class AzureEventGridSenderTests
+    {
+        private static string _domainEndpoint;
+        private static string _domainKey;
+
+        public AzureEventGridSenderTests()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("09f024f8-e8d1-4b78-9ddd-da941692e8fa")
+                .Build();
+
+            _domainEndpoint = config["MessageBroker:AzureEventGrid:DomainEndpoint"];
+            _domainKey = config["MessageBroker:AzureEventGrid:DomainKey"];
+        }
+
+        [Fact]
+        public async Task SendAsync_Success()
+        {
+            var message = new { Id = Guid.NewGuid() };
+            var metaData = new MetaData { };
+            var azureQueueSender = new AzureEventGridSender<object>(_domainEndpoint, _domainKey, "integration-test");
+            await azureQueueSender.SendAsync(message, metaData);
+        }
+    }
+}
