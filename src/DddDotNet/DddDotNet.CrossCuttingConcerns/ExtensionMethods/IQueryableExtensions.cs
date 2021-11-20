f1c584612bbee5cc5c9636a631b0a5d91ad70d1a
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-11-20 13:54:06 +0700

    Extension Methods

--- src/DddDotNet/DddDotNet.CrossCuttingConcerns/ExtensionMethods/IQueryableExtensions.cs
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/ExtensionMethods/IQueryableExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Linq.Expressions;
 
 namespace DddDotNet.CrossCuttingConcerns.ExtensionMethods
 {
@@ -10,6 +9,11 @@ namespace DddDotNet.CrossCuttingConcerns.ExtensionMethods
         public static IQueryable<T> Paged<T>(this IQueryable<T> source, int page, int pageSize)
         {
             return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+        {
+            return condition ? query.Where(predicate) : query;
         }
     }
 }
