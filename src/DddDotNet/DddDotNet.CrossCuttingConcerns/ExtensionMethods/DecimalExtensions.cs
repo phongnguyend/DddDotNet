0d12bf3723d46559bca3290f1708cb0cc73f3cfc
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-01-22 14:12:59 +0700

    useful extension methods

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/ExtensionMethods/DecimalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DddDotNet.CrossCuttingConcerns.ExtensionMethods
+{
+    public static class DecimalExtensions
+    {
+        public static decimal Limit(this decimal value, decimal min, decimal max)
+        {
+            return Math.Min(Math.Max(value, min), max);
+        }
+    }
+}
---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/ExtensionMethods/IntExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DddDotNet.CrossCuttingConcerns.ExtensionMethods
+{
+    public static class IntExtensions
+    {
+        public static int Limit(this int value, int min, int max)
+        {
+            return Math.Min(Math.Max(value, min), max);
+        }
+    }
+}
