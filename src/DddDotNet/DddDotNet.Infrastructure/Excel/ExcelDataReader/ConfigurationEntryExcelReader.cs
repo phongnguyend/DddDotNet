965b601458ef762945c367480e4f70cd447b515a
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-06-29 11:04:55 +0700

    (#11) [Excel] Read/Write Excel

---
+++ src/DddDotNet/DddDotNet.Infrastructure/Excel/ExcelDataReader/ConfigurationEntryExcelReader.cs
@@ -0,0 +1,71 @@
+﻿using DddDotNet.CrossCuttingConcerns.Excel;
+using DddDotNet.Domain.Entities;
+using ExcelDataReader;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DddDotNet.Infrastructure.Excel.ExcelDataReader
+{
+    public class ConfigurationEntryExcelReader : IExcelReader<List<ConfigurationEntry>>
+    {
+        public List<ConfigurationEntry> Read(Stream stream)
+        {
+            var rows = new List<ConfigurationEntry>();
+            var reader = ExcelReaderFactory.CreateReader(stream);
+            do
+            {
+                if (reader.VisibleState == "hidden")
+                {
+                    continue;
+                }
+
+                var sheetName = reader.Name;
+                if (sheetName == "IgnoredSheet")
+                {
+                    continue;
+                }
+
+                var rowIndex = 0;
+                var headerIndex = 0;
+                while (reader.Read())
+                {
+                    if (rowIndex < headerIndex)
+                    {
+                    }
+                    else if (rowIndex == headerIndex)
+                    {
+                        var headers = new List<string>();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            headers.Add(reader.GetValue(i)?.ToString());
+                        }
+
+                        // TODO: Validate Headers
+                    }
+                    else
+                    {
+                        rows.Add(new ConfigurationEntry
+                        {
+                            Key = reader.GetValue(0)?.ToString(),
+                            Value = reader.GetValue(1)?.ToString(),
+                        });
+                    }
+
+                    rowIndex++;
+                }
+            }
+            while (reader.NextResult());
+
+            return rows;
+        }
+
+        private static Dictionary<string, string> GetCorrectHeaders()
+        {
+            return new Dictionary<string, string>
+            {
+                { "A", "Key" },
+                { "B", "Value" },
+            };
+        }
+    }
+}
---
+++ src/DddDotNet/DddDotNet.IntegrationTests/Infrastructure/Excel/ExcelDataReader/ConfigurationEntryExcelTests.cs
@@ -0,0 +1,26 @@
+﻿using DddDotNet.Infrastructure.Excel.ExcelDataReader;
+using System.IO;
+using Xunit;
+
+namespace DddDotNet.IntegrationTests.Infrastructure.Excel.ExcelDataReader
+{
+    public class ConfigurationEntryExcelTests
+    {
+        [Fact]
+        public void Read_ReturnData()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            ConfigurationEntryExcelReader reader = new ConfigurationEntryExcelReader();
+            
+            using var fileStream = File.OpenRead("Infrastructure/Excel/ConfigurationEntries.xlsx");
+            var entries = reader.Read(fileStream);
+            
+            Assert.Equal(8, entries.Count);
+            Assert.Equal("Key1", entries[0].Key);
+            Assert.Equal("Value 1", entries[0].Value);
+            Assert.Equal("Key8", entries[7].Key);
+            Assert.Equal("Value 8", entries[7].Value);
+        }
+    }
+}
