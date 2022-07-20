b6b187e87140a2a0818dabf782d21dbd02d76210
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-07-20 18:29:28 +0700

    stream extension

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/ExtensionMethods/StreamExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace DddDotNet.CrossCuttingConcerns.ExtensionMethods
+{
+    public static class StreamExtensions
+    {
+        public static void ResetPosition(this Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+        }
+    }
+}
