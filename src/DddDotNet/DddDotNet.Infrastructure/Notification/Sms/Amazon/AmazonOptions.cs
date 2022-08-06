10dee8fa79221e16261ecf2c4b064eae632dd282
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-08-07 00:34:54 +0700

    (#19) [Sending SMS] Amazon Simple Notification Service (SNS)

---
+++ src/DddDotNet/DddDotNet.Infrastructure/Notification/Sms/Amazon/AmazonOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace DddDotNet.Infrastructure.Notification.Sms.Amazon
+{
+    public class AmazonOptions
+    {
+        public string AccessKeyID { get; set; }
+
+        public string SecretAccessKey { get; set; }
+
+        public string RegionEndpoint { get; set; }
+    }
+}
---
+++ src/DddDotNet/DddDotNet.Infrastructure/Notification/Sms/Amazon/AmazonSmsNotification.cs
@@ -0,0 +1,29 @@
+﻿using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DddDotNet.Infrastructure.Notification.Sms.Amazon
+{
+    public class AmazonSmsNotification : ISmsNotification
+    {
+        private readonly AmazonOptions _options;
+
+        public AmazonSmsNotification(AmazonOptions options)
+        {
+            _options = options;
+        }
+
+        public async Task SendAsync(ISmsMessage smsMessage, CancellationToken cancellationToken = default)
+        {
+            var snsClient = new AmazonSimpleNotificationServiceClient(_options.AccessKeyID, _options.SecretAccessKey, RegionEndpoint.GetBySystemName(_options.RegionEndpoint));
+
+            var publishResponse = await snsClient.PublishAsync(new PublishRequest
+            {
+                Message = smsMessage.Message,
+                PhoneNumber = smsMessage.PhoneNumber,
+            }, cancellationToken);
+        }
+    }
+}
