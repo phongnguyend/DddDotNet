160cdbcb8091412b565d32632f2115c8d179031a
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-08-16 11:44:40 +0700

    Object Pool

---
+++ src/DddDotNet/DddDotNet.Infrastructure/ObjectPools/StringBuilderPool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+
+namespace DddDotNet.Infrastructure.ObjectPools
+{
+    public class StringBuilderPool
+    {
+        private readonly ConcurrentStack<StringBuilder> _stack = new ();
+
+        public StringBuilder Get(int capacityHint)
+        {
+            if (!_stack.TryPop(out StringBuilder result))
+            {
+                result = new StringBuilder(capacityHint);
+            }
+
+            return result;
+        }
+
+        public void Return(StringBuilder builder)
+        {
+            builder.Clear();
+            _stack.Push(builder);
+        }
+    }
+}
