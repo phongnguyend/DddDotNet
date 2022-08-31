9bed66d7ba15427b965843dd375ac8a428ccb06f
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-08-31 09:39:10 +0700

    Value Objects: DateTimeRange & DateTimeOffsetRange

---
+++ src/DddDotNet/DddDotNet.Domain/ValueObjects/DateTimeRange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DddDotNet.Domain.ValueObjects
+{
+    public class DateTimeRange : ValueObject
+    {
+        public DateTime Start { get; private set; }
+
+        public DateTime End { get; private set; }
+
+        public DateTimeRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public DateTimeRange(DateTime start, TimeSpan duration)
+            : this(start, start.Add(duration))
+        {
+        }
+
+        public bool Overlaps(DateTimeRange dateTimeRange)
+        {
+            return Start < dateTimeRange.End && End > dateTimeRange.Start;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Start;
+            yield return End;
+        }
+    }
+}
