c3dd28b5c1b655ccf97bde1a464f76c94cfa9622
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-11-11 12:57:56 +0700

    (#21) Hashicorp Vault

---
+++ src/DddDotNet/DddDotNet.IntegrationTests/Infrastructure/Configurations/HashiCorpVaultConfigurationProviderTests.cs
@@ -0,0 +1,49 @@
+﻿using DddDotNet.Infrastructure.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DddDotNet.IntegrationTests.Infrastructure.Configurations
+{
+    public class HashiCorpVaultConfigurationProviderTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+        IConfigurationRoot _config;
+
+        public HashiCorpVaultConfigurationProviderTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+
+            // vault server -dev -dev-root-token-id=dev-only-token
+
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("09f024f8-e8d1-4b78-9ddd-da941692e8fa")
+                .AddHashiCorpVault(new HashiCorpVaultOptions
+                {
+                    Address = "http://localhost:8200/",
+                    SecretEnginePath = "secret",
+                    SecretPath = "DddDotNet",
+                    AuthMethod = "Token",
+                    Auth = new HashiCorpVaultAuthOptions
+                    {
+                        Token = new HashiCorpVaultTokenAuthOptions { Token = "dev-only-token" }
+                    }
+                })
+                .Build();
+        }
+
+        [Fact]
+        public Task Test()
+        {
+            var storageProvider = _config["Storage:Provider"];
+            var messageBokerProvider = _config["MessageBroker:Provider"];
+
+            _testOutputHelper.WriteLine($"Storage:Provider { storageProvider}");
+            _testOutputHelper.WriteLine($"MessageBroker:Provider {messageBokerProvider}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
