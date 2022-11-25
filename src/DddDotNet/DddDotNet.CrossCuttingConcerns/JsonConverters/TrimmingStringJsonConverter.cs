1cc683ac0a4bb795d1fa6c9a85fbd0b29dda5b1d
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-11-25 14:09:09 +0700

    Json Converter: Trimming String

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/JsonConverters/TrimmingStringJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DddDotNet.CrossCuttingConcerns.JsonConverters
+{
+    public class TrimmingStringJsonConverter : JsonConverter<string>
+    {
+        public override string Read(
+                ref Utf8JsonReader reader,
+                Type typeToConvert,
+                JsonSerializerOptions options) => reader.GetString()?.Trim();
+
+        public override void Write(
+                Utf8JsonWriter writer,
+                string value,
+                JsonSerializerOptions options) => writer.WriteStringValue(value?.Trim());
+    }
+}
