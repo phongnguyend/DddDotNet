598e05dc881e3e1bb2d5b8f15e9ac7ec9b6f1078
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-12-03 08:58:38 +0700

    create zip file from streams

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/IO/IZipFileService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DddDotNet.CrossCuttingConcerns.IO
+{
+    public interface IZipFileService
+    {
+        void CreateFromStreams(Dictionary<string, Stream> input, Stream output);
+
+        Stream CreateFromStreams(Dictionary<string, Stream> input);
+    }
+}
---
+++ src/DddDotNet/DddDotNet.Infrastructure/IO/ZipFileService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using DddDotNet.CrossCuttingConcerns.IO;
+
+namespace DddDotNet.Infrastructure.IO
+{
+    public class ZipFileService : IZipFileService
+    {
+        public void CreateFromStreams(Dictionary<string, Stream> input, Stream output)
+        {
+            using (ZipArchive archive = new ZipArchive(output, ZipArchiveMode.Update, true))
+            {
+                foreach (var file in input)
+                {
+                    ZipArchiveEntry entry = archive.CreateEntry(file.Key);
+                    using var entryStream = entry.Open();
+                    file.Value.CopyTo(entryStream);
+                }
+            }
+
+            output.Position = 0;
+        }
+
+        public Stream CreateFromStreams(Dictionary<string, Stream> input)
+        {
+            var output = CreateTempFileStream();
+
+            CreateFromStreams(input, output);
+
+            output.Position = 0;
+            return output;
+        }
+
+        public static FileStream CreateTempFileStream()
+        {
+            return new FileStream(Path.GetTempFileName(), FileMode.Open, FileAccess.ReadWrite, FileShare.None, 4096, FileOptions.DeleteOnClose);
+        }
+    }
+}
