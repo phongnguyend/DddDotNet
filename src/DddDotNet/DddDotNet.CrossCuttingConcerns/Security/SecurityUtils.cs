edace00c09afa54719147e11186a96906313298b
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-03-14 23:30:26 +0700

    Prevent CRLF Injection

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/Security/SecurityUtils.cs
@@ -0,0 +1,25 @@
+﻿namespace DddDotNet.CrossCuttingConcerns.Security
+{
+    public static class SecurityUtils
+    {
+        private static string ReplaceCRLF(this string text, string replacementCR, string replacementLF)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+
+            return text.Replace("\r", replacementCR).Replace("\n", replacementLF);
+        }
+
+        public static string ReplaceCRLF(this string text)
+        {
+            return ReplaceCRLF(text, " ", " ");
+        }
+
+        public static string RemoveCRLF(this string text)
+        {
+            return ReplaceCRLF(text, string.Empty, string.Empty);
+        }
+    }
+}
