6646f5afc84bb09b7b29dae34ba3064e89164a30
Phong Nguyen <nguyendoanphong.it@gmail.com>
2023-07-08 09:54:49 +0700

    (#23) Message Bus Encryption: RabbitMQ

--- src/DddDotNet/DddDotNet.Infrastructure/MessageBrokers/RabbitMQ/RabbitMQReceiverOptions.cs
+++ src/DddDotNet/DddDotNet.Infrastructure/MessageBrokers/RabbitMQ/RabbitMQReceiverOptions.cs
@@ -1,27 +1,26 @@
-﻿namespace DddDotNet.Infrastructure.MessageBrokers.RabbitMQ
+﻿namespace DddDotNet.Infrastructure.MessageBrokers.RabbitMQ;
+
+public class RabbitMQReceiverOptions
 {
-    public class RabbitMQReceiverOptions
-    {
-        public string HostName { get; set; }
+    public string HostName { get; set; }
 
-        public string UserName { get; set; }
+    public string UserName { get; set; }
 
-        public string Password { get; set; }
+    public string Password { get; set; }
 
-        public string QueueName { get; set; }
+    public string QueueName { get; set; }
 
-        public bool AutomaticCreateEnabled { get; set; }
+    public bool AutomaticCreateEnabled { get; set; }
 
-        public string QueueType { get; set; }
+    public string QueueType { get; set; }
 
-        public string ExchangeName { get; set; }
+    public string ExchangeName { get; set; }
 
-        public string RoutingKey { get; set; }
+    public string RoutingKey { get; set; }
 
-        public bool SingleActiveConsumer { get; set; }
+    public bool SingleActiveConsumer { get; set; }
 
-        public bool MessageEncryptionEnabled { get; set; }
+    public bool MessageEncryptionEnabled { get; set; }
 
-        public string MessageEncryptionKey { get; set; }
-    }
+    public string MessageEncryptionKey { get; set; }
 }
