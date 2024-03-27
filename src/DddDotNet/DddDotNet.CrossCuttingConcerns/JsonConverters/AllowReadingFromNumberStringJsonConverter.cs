d26d72ff3bea662ee7a7c53fa1775f908fcdf802
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-03-27 17:25:58 +0700

    json converters: allow reading string from number

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/JsonConverters/AllowReadingFromNumberStringJsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DddDotNet.CrossCuttingConcerns.JsonConverters
+{
+    public class AllowReadingFromNumberStringJsonConverter : JsonConverter<string>
+    {
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                reader.GetDecimal().ToString();
+            }
+
+            return reader.GetString();
+
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) => writer.WriteStringValue(value);
+    }
+}
