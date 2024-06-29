2af68e267deb487f4fc9e67a8366bc24cacb43cd
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-06-29 11:15:13 +0700

    Ensure Valid Directory/File Name

--- src/DddDotNet/DddDotNet.CrossCuttingConcerns/Security/SecurityUtils.cs
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/Security/SecurityUtils.cs
@@ -1,7 +1,12 @@
-﻿namespace DddDotNet.CrossCuttingConcerns.Security
+﻿using System.IO;
+using System.Linq;
+
+namespace DddDotNet.CrossCuttingConcerns.Security
 {
     public static class SecurityUtils
     {
+        private static readonly char[] InvalidFileNameChars = Path.GetInvalidFileNameChars();
+
         private static string ReplaceCRLF(this string text, string replacementCR, string replacementLF)
         {
             if (string.IsNullOrWhiteSpace(text))
@@ -20,6 +25,21 @@
         public static string RemoveCRLF(this string text)
         {
             return ReplaceCRLF(text, string.Empty, string.Empty);
+        }
+
+        public static string EnsureValidDirectoryName(this string name)
+        {
+            return EnsureValidFileName(name);
+        }
+
+        public static string EnsureValidFileName(this string name)
+        {
+            if (name.Any(c => InvalidFileNameChars.Contains(c)))
+            {
+                throw new IOException("Invalid Characters");
+            }
+
+            return name;
         }
     }
 }
