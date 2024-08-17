f581e27804869f679e12217474739c4568363b38
Phong Nguyen <nguyendoanphong.it@gmail.com>
2024-08-17 09:27:31 +0700

    combine uri paths

---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns.UnitTests/UriPathTests.cs
@@ -0,0 +1,72 @@
+﻿using DddDotNet.CrossCuttingConcerns.Uris;
+
+namespace DddDotNet.CrossCuttingConcerns.UnitTests;
+
+public class UriPathTests
+{
+    [Fact]
+    public void Combine_OnePath_ReturnTheOriginal()
+    {
+        var url = UriPath.Combine("https://stackoverflow.com/questions/372865");
+        Assert.Equal("https://stackoverflow.com/questions/372865", url);
+    }
+
+    [Fact]
+    public void Combine_OnePath_ShouldNotRemoveTheSlashAtTheEnd()
+    {
+        var url = UriPath.Combine("https://stackoverflow.com/questions/372865/");
+        Assert.Equal("https://stackoverflow.com/questions/372865/", url);
+    }
+
+    [Theory]
+    [InlineData("https://stackoverflow.com/questions/372865", "path-combine-for-urls")]
+    [InlineData("https://stackoverflow.com/questions/372865", "/path-combine-for-urls")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "path-combine-for-urls")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "/path-combine-for-urls")]
+    public void Combine_TwoPaths_ReturnSameResult(string uri1, string uri2)
+    {
+        var url = UriPath.Combine(uri1, uri2);
+        Assert.Equal("https://stackoverflow.com/questions/372865/path-combine-for-urls", url);
+    }
+
+    [Theory]
+    [InlineData("https://stackoverflow.com/questions/372865", "path-combine-for-urls/")]
+    [InlineData("https://stackoverflow.com/questions/372865", "/path-combine-for-urls/")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "path-combine-for-urls/")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "/path-combine-for-urls/")]
+    public void Combine_TwoPaths_ShouldNotRemoveTheSlashAtTheEnd(string uri1, string uri2)
+    {
+        var url = UriPath.Combine(uri1, uri2);
+        Assert.Equal("https://stackoverflow.com/questions/372865/path-combine-for-urls/", url);
+    }
+
+    [Theory]
+    [InlineData("https://stackoverflow.com/questions/372865", "path-combine-for-urls", "xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865", "path-combine-for-urls", "/xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865", "/path-combine-for-urls", "xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865", "/path-combine-for-urls", "/xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "path-combine-for-urls", "xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "path-combine-for-urls", "/xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "/path-combine-for-urls", "xxx")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "/path-combine-for-urls", "/xxx")]
+    public void Combine_ThreePaths_ReturnSameResult(string uri1, string uri2, string uri3)
+    {
+        var url = UriPath.Combine(uri1, uri2, uri3);
+        Assert.Equal("https://stackoverflow.com/questions/372865/path-combine-for-urls/xxx", url);
+    }
+
+    [Theory]
+    [InlineData("https://stackoverflow.com/questions/372865", "path-combine-for-urls", "xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865", "path-combine-for-urls", "/xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865", "/path-combine-for-urls", "xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865", "/path-combine-for-urls", "/xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "path-combine-for-urls", "xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "path-combine-for-urls", "/xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "/path-combine-for-urls", "xxx/")]
+    [InlineData("https://stackoverflow.com/questions/372865/", "/path-combine-for-urls", "/xxx/")]
+    public void Combine_ThreePaths_ShouldNotRemoveTheSlashAtTheEnd(string uri1, string uri2, string uri3)
+    {
+        var url = UriPath.Combine(uri1, uri2, uri3);
+        Assert.Equal("https://stackoverflow.com/questions/372865/path-combine-for-urls/xxx/", url);
+    }
+}
---
+++ src/DddDotNet/DddDotNet.CrossCuttingConcerns/Uris/UriPath.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace DddDotNet.CrossCuttingConcerns.Uris
+{
+    public static class UriPath
+    {
+        public static string Combine(params string[] paths)
+        {
+            return paths.Aggregate((current, path) => $"{current.TrimEnd('/')}/{path.TrimStart('/')}");
+        }
+    }
+}
